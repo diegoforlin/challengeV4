e35c8110c5b9ded96ac3442d27097a5134c96111
diegoforlin <135170390+diegoforlin@users.noreply.github.com>
2024-02-28 13:57:00 -0300

    Delete src directory

--- src/main/java/com/challengescrd/challenge/entities/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challengescrd.challenge.entities;
-
-import com.challengescrd.challenge.address.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address,Long> {
-}
